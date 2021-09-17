2ae0c05633a2e5029bcc532468f9b4830b13454c
Genaro-Coronel-Gtz <sk_8_917@hotmail.com>
2021-09-17 16:12:33 -0500

    Fix[script]: fix script issues

--- script.sh
+++ script.sh
@@ -39,34 +39,8 @@ sed -i "s/JVB_AUTH_PASSWORD=JVB_AUTH_PASSWORD/JVB_AUTH_PASSWORD=$JVB_PASSWORD/" 
 
 mkdir -p /home/$USER/video_bridge/jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
 
-#Setear ip publica e ip privada en xmpp_Server, docker_host ,xmpp_bosh
-./gen-passwords.sh
-
 sudo -u $USER /usr/local/bin/docker-compose pull
 sudo -u $USER /usr/local/bin/docker-compose up -d
-sleep 60
-sudo -u $USER /usr/local/bin/docker-compose stop
-
-
-echo "config.disableDeepLinking = true;" > jitsi-meet-cfg/web/custom-config.js
-# para registrar mas servidores stun
-echo "config.p2p.stunServers = [{urls: 'stun:meet-jit-si-turnrelay.jitsi.net:443'},{urls: 'stun:stun.l.google.com:19302'},{urls: 'stun:stun1.l.google.com:19302'},{urls: 'stun:stun2.l.google.com:19302'},{urls: 'stun:stun3.l.google.com:19302'},{urls: 'stun:stun4.l.google.com:19302'}];" >> jitsi-meet-cfg/web/custom-config.js
-# para desactivar opciones no deseadas
-echo "config.toolbarButtons = ['microphone', 'camera', 'desktop', 'hangup', 'chat', 'select-background', 'videobackgroundblur'];" >> jitsi-meet-cfg/web/custom-config.js
-
-# para limitar a una sola conexion
-sed -i '1imuc_max_occupants=2\nmuc_access_whitelist= {\n    "focus@auth.meet.jitsi",\n    "jvb@auth.meet.jitsi"\n}' jitsi-meet-cfg/prosody/config/conf.d/jitsi-meet.cfg.lua
-sed -i '114i\ \ \ \ \ \ \ \ "muc_max_occupants";\n' jitsi-meet-cfg/prosody/config/conf.d/jitsi-meet.cfg.lua
-# configurar redireccion al terminar la llamada
-sed -i -e '$alocation = / {\n    return 302 https://www.nimbo-x.com/teleconsulta-completada;\n}\n' jitsi-meet-cfg/web/nginx/meet.conf
-
-# Hacemos inmutable meet.conf para que no se pierda la configuracion cada que se reinicien los contenedores
-
-chattr +i /home/$USER/video_bridge/jitsi-meet-cfg/prosody/config/conf.d/jitsi-meet.cfg.lua
-chattr +i /home/$USER/video_bridge/jitsi-meet-cfg/web/nginx/meet.conf
-
-# Levantar los containers de jitsi
-cd /home/$USER/video_bridge
 sudo -u $USER /usr/local/bin/docker-compose start
 
 # Eliminar el archivo zip del repo de jitsi_main
